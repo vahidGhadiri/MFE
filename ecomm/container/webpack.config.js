e1850bf78ac7ee4d0ce947afc19810a2a9526823
vahidGhadiri <whydrf@gmail.com>
2024-08-09 23:22:22 +0330

    refactoring

--- ecomm/container/webpack.config.js
+++ ecomm/container/webpack.config.js
@@ -2,20 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        port: 8080,
-    },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                products: 'products@http://localhost:8081/remoteEntry.js',
-                cart: 'cart@http://localhost:8082/remoteEntry.js'
-            },
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-    ],
+  mode: 'development',
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        products: 'products@http://localhost:8081/remoteEntry.js',
+        cart: 'cart@http://localhost:8082/remoteEntry.js',
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
